aecaeb7e32981f6346f4fb6797d3eaad997c9e1c
ArabNinja-hub <ronisherbwalya@gmail.com>
2026-07-10 17:21:04 +0200

    Delete supabase directory

--- supabase/migrations/20260707105559_2d1a586d-ef78-44bc-b7bc-aff04c6c3012.sql
+++
@@ -1,3 +0,0 @@
-
-REVOKE EXECUTE ON FUNCTION public.has_role(uuid, public.app_role) FROM public, anon, authenticated;
-REVOKE EXECUTE ON FUNCTION public.handle_new_user() FROM public, anon, authenticated;
